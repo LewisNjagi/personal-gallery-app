a9ffbc654ed17cd012a3adfac95de94b2fc0ad76
Lewis Njagi <lewismaybach@gmail.com>
2021-03-20 20:44:18 +0300

    add category model

--- personal/models.py
+++ personal/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
+
+class category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
