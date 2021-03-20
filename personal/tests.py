64918a78fa46f2e67f7f47c7c5ce9637bce93c5f
Lewis Njagi <lewismaybach@gmail.com>
2021-03-20 21:06:40 +0300

    test save image

--- personal/tests.py
+++ personal/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Location,Image,Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(name = '')
